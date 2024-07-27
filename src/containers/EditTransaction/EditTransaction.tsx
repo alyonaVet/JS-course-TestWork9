8bf6b06d2f7d82c3f983b3dd1c73deac0c2475c0
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 18:53:46 +0300

    created container EditTransaction;

---
+++ src/containers/EditTransaction/EditTransaction.tsx
@@ -0,0 +1,11 @@
+import AddTransaction from '../AddTransaction/AddTransaction';
+
+const EditTransaction = () => {
+  return (
+    <div>
+      <AddTransaction />
+    </div>
+  );
+};
+
+export default EditTransaction;
