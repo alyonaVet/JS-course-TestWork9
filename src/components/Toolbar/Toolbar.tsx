1d569bda443366a832a9d63edf9f56059de5e0c6
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 18:53:16 +0300

    refactor Toolbar; added loader;

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,12 +1,42 @@
 import {NavLink} from 'react-router-dom';
+import {
+  selectCreatTransactionsLoading,
+  selectDeleteTransactionsLoading, selectFetchTransactionsLoading, selectUpdateTransactionLoading
+} from '../../features/transaction/transactionSlice';
+import {useAppSelector} from '../../app/hooks';
+import {
+  selectCreatCategoriesLoading, selectDeleteCategoryLoading,
+  selectFetchCategoriesLoading,
+  selectUpdateCategoryLoading
+} from '../../features/category/categorySlice';
 
 const Toolbar = () => {
+  const transactionsLoading = useAppSelector(selectDeleteTransactionsLoading);
+  const creatTransactionsLoading = useAppSelector(selectCreatTransactionsLoading);
+  const fetchTransactionsLoading = useAppSelector(selectFetchTransactionsLoading);
+  const updateTransactionLoading = useAppSelector(selectUpdateTransactionLoading);
+
+  const creatCategoriesLoading = useAppSelector(selectCreatCategoriesLoading);
+  const fetchCategoriesLoading = useAppSelector(selectFetchCategoriesLoading);
+  const updateCategoryLoading = useAppSelector(selectUpdateCategoryLoading);
+  const deleteCategoryLoading = useAppSelector(selectDeleteCategoryLoading);
+
+  const somethingLoading =
+    transactionsLoading ||
+    creatTransactionsLoading ||
+    fetchTransactionsLoading ||
+    updateTransactionLoading ||
+    creatCategoriesLoading ||
+    fetchCategoriesLoading ||
+    updateCategoryLoading ||
+    deleteCategoryLoading;
+
 
   return (
     <nav className="navbar navbar-dark bg-primary">
       <div className="container d-flex align-items-center justify-content-between">
         <div className="d-flex align-items-center">
-          <NavLink to="/" className="navbar-brand">Finance Tracker</NavLink>
+          <NavLink to="/" className="navbar-brand">Finance Tracker{somethingLoading ? " loading..." : ""}</NavLink>
         </div>
         <ul className="navbar-nav d-flex flex-row gap-3 flex-nowrap">
           <li className="nav-item">
