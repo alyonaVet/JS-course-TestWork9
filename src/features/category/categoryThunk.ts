2d018130a916d6618208dfcddd60b69abf297283
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 14:56:30 +0300

    added update category;

--- src/features/category/categoryThunk.ts
+++ src/features/category/categoryThunk.ts
@@ -1,11 +1,11 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
-import {ApiCategories, ApiCategoryType, CategoryType} from '../../types';
+import {ApiCategories, ApiCategoryType, CategoryType, UpdateCategoryType} from '../../types';
 import {RootState} from '../../app/store';
 import axiosApi from '../../axiosApi';
 
 export const createCategory = createAsyncThunk<void, ApiCategoryType, { state: RootState }>(
   'categories/create',
-  async (apiCategory : ApiCategoryType) => {
+  async (apiCategory: ApiCategoryType) => {
     await axiosApi.post('/categories.json', apiCategory);
   },
 );
@@ -21,6 +21,20 @@ export const fetchCategories = createAsyncThunk<CategoryType[], void, { state: R
     return Object.keys(categories).map((id) => ({
       id,
       ...categories[id],
-    }))
+    }));
   }
+);
+
+export const updateCategory = createAsyncThunk<void, UpdateCategoryType, { state: RootState }>(
+  'categories/update',
+  async ({id, category}) => {
+    await axiosApi.put(`/categories/${id}.json`, category);
+  },
+);
+
+export const deleteCategory = createAsyncThunk<void, string, { state: RootState }>(
+  'categories/delete',
+  async (categoryID) => {
+    await axiosApi.delete('/categories/' + categoryID + '.json');
+  },
 );
