7d70ad1ed0ec934a087c1f3ec8dad8abd56f7b6d
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 14:55:31 +0300

    added state transaction;

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {categoryReducer} from '../features/category/categorySlice';
+import {transactionReducer} from '../features/transaction/transactionSlice';
 
 export const store = configureStore({
   reducer: {
     category: categoryReducer,
+    transaction: transactionReducer,
   },
 });
 
