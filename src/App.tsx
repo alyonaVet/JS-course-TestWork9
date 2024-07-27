90bc3cd494c70eb5937802b21148f2c3030e40d9
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 17:04:27 +0300

    added routes; useEffect;

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import Toolbar from './components/Toolbar/Toolbar';
+import {Route, Routes} from 'react-router-dom';
+import MainPage from './containers/MainPage/MainPage';
+import Categories from './containers/Categories/Categories';
+import AddTransaction from './containers/AddTransaction/AddTransaction';
+import {useEffect} from 'react';
+import {useAppDispatch} from './app/hooks';
+import {fetchCategories} from './features/category/categoryThunk';
+
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
     return (
         <>
-            Hello World!
+          <header>
+            <Toolbar />
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/add-transaction" element={<AddTransaction />} />
+            </Routes>
+          </main>
         </>
     );
 };
