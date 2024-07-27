fadb1509a9b53f0223a6616f5cf9ab0e8ba0d60e
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 17:02:39 +0300

    added AddTransactionForm;

--- src/containers/AddTransaction/AddTransaction.tsx
+++ src/containers/AddTransaction/AddTransaction.tsx
@@ -1,7 +1,18 @@
-const AddTransaction = () => {
-  return (
-    <div>
+import AddTransactionForm from '../../components/AddForm/AddTransactionForm/AddTransactionForm';
+import {useEffect} from 'react';
+import {fetchCategories} from '../../features/category/categoryThunk';
+import {useAppDispatch} from '../../app/hooks';
 
+const AddTransaction = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <AddTransactionForm isLoading={false} />
     </div>
   );
 };
