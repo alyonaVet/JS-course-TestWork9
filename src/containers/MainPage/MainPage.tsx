c57935c60faef858fd4c698db760832190167fa2
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 18:55:14 +0300

    refactor MainPage;

--- src/containers/MainPage/MainPage.tsx
+++ src/containers/MainPage/MainPage.tsx
@@ -1,87 +1,100 @@
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
-import {selectTransactions} from '../../features/transaction/transactionSlice';
+import {
+  selectFetchTransactionsLoading,
+  selectTransactions
+} from '../../features/transaction/transactionSlice';
 import {useEffect} from 'react';
 import {deleteTransaction, fetchTransaction} from '../../features/transaction/transactionThunk';
 import {selectCategories} from '../../features/category/categorySlice';
 import {TransactionType} from '../../types';
 import dayjs from 'dayjs';
+import {useNavigate} from 'react-router-dom';
+import Spinner from '../../components/Spinners/Spinner';
 
 const MainPage = () => {
 
-    const transactions = useAppSelector(selectTransactions);
-    const categories = useAppSelector(selectCategories);
+  const transactions = useAppSelector(selectTransactions);
+  const transactionLoading = useAppSelector(selectFetchTransactionsLoading);
+  const categories = useAppSelector(selectCategories);
+  const navigate = useNavigate();
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const deleteHandler = async (transactionId: string) => {
-      try {
-        await dispatch(deleteTransaction(transactionId));
-        await dispatch(fetchTransaction());
-      } catch (error) {
-        console.error('Could not delete transaction!', error);
-      }
-    };
-
-    useEffect(() => {
-      dispatch(fetchTransaction());
-    }, [dispatch]);
-
-    const getTransactionCategory = (transaction: TransactionType) => categories.find((c) => c.id === transaction.category);
-
-    let totalAmount = 0;
+  const deleteHandler = async (transactionId: string) => {
+    try {
+      await dispatch(deleteTransaction(transactionId));
+      await dispatch(fetchTransaction());
+    } catch (error) {
+      console.error('Could not delete transaction!', error);
+    }
+  };
 
 
-    return (
-      <div className="container mt-5">
+  useEffect(() => {
+    dispatch(fetchTransaction());
+  }, [dispatch]);
 
-        {
-          Object.entries(transactions)
-            .map(
-              ([transactionId, transaction]) => {
-                const category = getTransactionCategory(transaction);
+  const getTransactionCategory = (transaction: TransactionType) => categories.find((c) => c.id === transaction.category);
 
-                if (category === undefined) {
-                  return null;
-                }
-
-                const isNegative = category.type === 'expense';
-
-                const sign = isNegative ? '-' : '+';
-                const color = isNegative ? 'text-danger' : 'text-success';
-
-                if (sign === '+') {
-                  totalAmount += transaction.amount;
-                } else {
-                  totalAmount -= transaction.amount;
-                }
+  let totalAmount = 0;
 
 
-                return (
-                  <div className="d-flex justify-content-between border rounded-3 mb-3 p-3" key={transactionId}>
-                    <div>{dayjs(transaction.createdAt).format('DD.MM.YYYY HH:mm:ss')}</div>
-                    <div>{category.name}</div>
-                    <div className={`${color}`}>{sign}{transaction.amount}</div>
-                    <div className="d-flex gap-3">
-                      <button type="button" className="btn btn-secondary">Edit</button>
-                      <button
-                        type="button"
-                        className="btn btn-danger"
-                        onClick={() => deleteHandler(transactionId)}
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </div>);
+  return (
+    <div className="container mt-5">
+      {transactionLoading ? (
+        <Spinner/>
+      ) : (
+        Object.entries(transactions)
+          .map(
+            ([transactionId, transaction]) => {
+              const category = getTransactionCategory(transaction);
+
+              if (category === undefined) {
+                return null;
               }
-            )
-        }
-        <div className="w-25 border rounded-1 p-3 text-center">Total: <span
-          className={totalAmount < 0 ? 'text-danger' : 'text-success'}>{totalAmount}</span></div>
 
-      </div>
-    )
-      ;
-  }
-;
+              const isNegative = category.type === 'expense';
+
+              const sign = isNegative ? '-' : '+';
+              const color = isNegative ? 'text-danger' : 'text-success';
+
+              if (sign === '+') {
+                totalAmount += transaction.amount;
+              } else {
+                totalAmount -= transaction.amount;
+              }
+              return (
+                <div className="d-flex justify-content-between border rounded-3 mb-3 p-3" key={transactionId}>
+                  <div>{dayjs(transaction.createdAt).format('DD.MM.YYYY HH:mm:ss')}</div>
+                  <div>{category.name}</div>
+                  <div className={`${color}`}>{sign}{transaction.amount}</div>
+                  <div className="d-flex gap-3">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => {
+                        navigate(`/edit-transaction/${transactionId}`);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger"
+                      onClick={() => deleteHandler(transactionId)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>);
+            }
+          )
+      )}
+      <div className="w-25 border rounded-1 p-3 text-center">Total: <span
+        className={totalAmount < 0 ? 'text-danger' : 'text-success'}>{totalAmount}</span></div>
+
+    </div>
+  );
+};
 
 export default MainPage;
