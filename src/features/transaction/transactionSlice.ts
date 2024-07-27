a43a6e9bbfd5787b03d26683c89e67e45d95ce46
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-27 17:03:36 +0300

    added fetchTransaction; deleteTransaction;

--- src/features/transaction/transactionSlice.ts
+++ src/features/transaction/transactionSlice.ts
@@ -1,20 +1,26 @@
-import {TransactionType} from '../../types';
+import {ApiTransactions} from '../../types';
 import {createSlice} from '@reduxjs/toolkit';
-import {createTransaction} from './transactionThunk';
+import {createTransaction, deleteTransaction, fetchTransaction} from './transactionThunk';
 
 export interface TransactionState {
-  transactions: TransactionType[];
+  transactions: ApiTransactions;
   createLoading: boolean;
+  fetchLoading: boolean;
+  deleteLoading: false | string;
+
 }
 
 const initialState: TransactionState = {
-  transactions: [],
+  transactions: {},
   createLoading: false,
+  fetchLoading: false,
+  deleteLoading: false,
+
 }
 
 
 const transactionSlice = createSlice({
-  name: 'transactions',
+  name: 'transaction',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
@@ -28,19 +34,48 @@ const transactionSlice = createSlice({
       .addCase(createTransaction.rejected, (state) => {
         state.createLoading = false;
       });
+    builder
+      .addCase(fetchTransaction.pending, (state) => {
+        state.fetchLoading = true;
+      })
+      .addCase(fetchTransaction.fulfilled, (state, action) => {
+        state.fetchLoading = false;
+        state.transactions = action.payload;
+      })
+      .addCase(fetchTransaction.rejected, (state) => {
+        state.fetchLoading = false;
+      });
+    builder
+      .addCase(deleteTransaction.pending, (state, { meta: { arg: category } }) => {
+        state.deleteLoading = category;
+      })
+      .addCase(deleteTransaction.fulfilled, (state) => {
+        state.deleteLoading = false;
+      })
+      .addCase(deleteTransaction.rejected, (state) => {
+        state.deleteLoading = false;
+      });
   },
   selectors: {
     selectTransactions: (state) => state.transactions,
+    selectDeleteTransactionsLoading: (state) => state.deleteLoading,
     selectCreatTransactionsLoading: (state) => state.createLoading,
+    selectFetchTransactionsLoading: (state) => state.fetchLoading,
+
   }
+
 });
 
 export const transactionReducer = transactionSlice.reducer;
 
 export const {
   selectTransactions,
+  selectDeleteTransactionsLoading,
   selectCreatTransactionsLoading,
+  selectFetchTransactionsLoading,
 } = transactionSlice.selectors;
+
+
 
 
 
